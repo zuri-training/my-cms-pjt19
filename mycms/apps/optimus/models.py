f4cb5b200e9364be71703f6933b3ea2905002d92
Dabztechwrta <healervic17@gmail.com>
2022-08-11 15:41:29 +0100

    Dabztechwta commit to optimus, >> portfolio views and model

--- mycms/apps/optimus/models.py
+++ mycms/apps/optimus/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Portfolio(models.Model):
+    
+    STATUS_CHOICE = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    # DB Fields
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='Portfolio Templates'
+    )
+    publish = models.DateTimeField(default=timezone.now)
+
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICE, default="draft"
+    )
+
+    class Meta:
+        ordering = ("-published",)
+
+    def save(self, *args, **Kwargs):
+        self.slug = slugify(self.author)
+        super().save(*args, **Kwargs)
+        pass
+
+    def __str__(self):
+        return self.author
--- mycms/apps/optimus/views.py
+++ mycms/apps/optimus/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Portfolio
+from django.core.serializers import serialize
+import json
 
 # Create your views here.
+def index(request):
+    portfolio = Portfolio.objects.all()
+    return render(request, 'honeylens.html', 'IRONHIDE.html', {"porfolio": portfolio})
+
+def addPortfolio(request):
+    render(request, 'honeylens.html', 'IRONHIDE.html')
+
+def savePortfolio(request):
+    if(request.method =='POST'):
+        html = request.POST['html']
+        css = request.POST['css']
+        portfolio = Portfolio.objects.create(names="untitled", html=html, css=css)
+        portfolio.save()
+    return JsonResponse[{ "result" : [json.loads(serialize('json', [portfolio]))][0]}]
+
+def editPortfolio(request, id):
+    portfolio = Portfolio.objects.get(pk=id)
+    return render(request, 'honeylens.html', 'IRONHIDE.html', {"portfolio": portfolio})
+
+def editPortfolioContent(request, id):
+    if(request.method =='POST'):
+        html = request.POST['html']
+        css = request.POST['css']
+        portfolio = Portfolio.objects.get(pk=id)
+        portfolio.html = html
+        portfolio.css = css
+        portfolio.save()
+    return JsonResponse[{ "result" : [json.loads(serialize('json', [portfolio]))][0]}]
+
+
